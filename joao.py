8a88b0d5a58eba302d27d262f990aed28c1fa5d5
Joao Victor De Oliveira <joao.oliveira@proway.treina>
2022-09-21 20:02:10 -0300

    "Fila de Espera"

--- joao.py
+++ joao.py
@@ -10,14 +10,14 @@ atendente = ['Xuxa', 'Shakira', 'Faustão', 'Ana Maria Braga']
 if escolha == 'Atendente': #se for escolhido falar com atendente, inicia o código
     for x in range(10, 0, -1):
         os.system('cls')
-        print(f'Você está na posição {x}') #diz a posição
+        print(f'Você está na posição {x}\n*música de fundo*') #diz a posição
         time.sleep(random.randint(0,5)) #possui intervalo aleatório entre uma posição e outra
     os.system('cls')
     print('Erro no sistema!\nEstamos reiniciando...')
     time.sleep(3)
     for x in range(random.randint(9,15), 0, -1): #a posição é aleatória entre 9 e 15
         os.system('cls')
-        print(f'Você está na posição {x}') #diz a posição
+        print(f'Você está na posição {x}\n*música de fundo*') #diz a posição
         time.sleep(random.randint(0,5)) #possui intervalo aleatório entre uma posição e outra
     os.system('cls')
-    print("Atendente {} irá te recepcionar!".format(random.choice(atendente))) #um atendente aleatório da lista será chamado
+    print("Atendente {} irá te recepcionar!".format(random.choice(atendente))) #um atendente aleatório da lista será chamados
